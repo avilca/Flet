459f5d95d501dab6e66bcfe68cdc4b95fbb3095b
avilca <avilca85@gmail.com>
2023-09-21 17:49:44 -0500

    Segundo archivo

---
+++ operacionesmatematicas.py
@@ -0,0 +1,44 @@
+import flet as ft
+
+def main(page):
+
+    def sumar(e):
+        txtresultado.value = str(int(valor1.value) +  int(valor2.value))
+        page.update()
+
+    def restar(e):
+        txtresultado.value = str(int(valor1.value) -  int(valor2.value))
+        page.update()    
+
+    page.add(ft.Text(value="Ingrese 1er valor", color="yellow", size=20))
+    page.update()
+    valor1 = ft.TextField(hint_text="", width=200)
+    page.add(valor1)
+
+    page.add(ft.Text(value="Ingrese 2do valor", color="yellow", size=20))
+    page.update()
+    valor2 =ft.TextField(hint_text="", width=200)
+    page.add(valor2)
+    
+    page.add(ft.ElevatedButton(
+        text="+", 
+        on_click = sumar,
+        data="+", 
+        )
+    )
+    page.update()
+
+    page.add(ft.ElevatedButton(
+        text="-", 
+        on_click = restar, 
+        data="-",
+        )
+    )
+    page.update()
+    
+
+    txtresultado = ft.TextField(hint_text="", width=200)
+    page.add(txtresultado)
+    page.update()
+
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)        
